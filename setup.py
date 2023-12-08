a58b2c16e4e00afbcfe70720156bde6ad6ceaf2b
Rachid Jeffali <rachid.jeffali@wassati.com>
2023-12-08 16:58:27 +0100

    creation de la wheel

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
         'watchdog'
     ],
     author="Rachid Jeffali",
-    description="Une bibliothèque de surveillance (watchdog) simple mais puissante pour les changements dans les fichiers et répertoires, avec une prise en charge avancée de la gestion d'événements."
+    description="Une bibliothèque de surveillance (watchdog) simple mais puissante pour les changements dans les fichiers et répertoires, avec une prise en charge avancée de la gestion d'événements.",
+    long_description='''Monitopy est une bibliothèque Python qui permet de surveiller les changements 
+                        dans les fichiers et répertoires, offrant une solution simple pour détecter 
+                        les créations, modifications et suppressions de fichiers.'''
 )
